c2d17a230a08883c46204c04de4152dd9d560066
PriscilaRonche <priscila.ronche@gmail.com>
2026-08-21 15:23:09 -0300

    Add files via upload

--- index.d.ts
+++ index.d.ts
@@ -1,77 +1,102 @@
 import { JimpClass } from "@jimp/types";
+import { z } from "zod";
+declare const MaskOptionsSchema: z.ZodUnion<[z.ZodObject<{
+    bitmap: z.ZodObject<{
+        data: z.ZodUnion<[z.ZodType<Buffer, z.ZodTypeDef, Buffer>, z.ZodType<Uint8Array, z.ZodTypeDef, Uint8Array>]>;
+        width: z.ZodNumber;
+        height: z.ZodNumber;
+    }, "strip", z.ZodTypeAny, {
+        data: Buffer | Uint8Array;
+        width: number;
+        height: number;
+    }, {
+        data: Buffer | Uint8Array;
+        width: number;
+        height: number;
+    }>;
+}, "strip", z.ZodTypeAny, {
+    bitmap: {
+        data: Buffer | Uint8Array;
+        width: number;
+        height: number;
+    };
+}, {
+    bitmap: {
+        data: Buffer | Uint8Array;
+        width: number;
+        height: number;
+    };
+}>, z.ZodObject<{
+    src: z.ZodObject<{
+        bitmap: z.ZodObject<{
+            data: z.ZodUnion<[z.ZodType<Buffer, z.ZodTypeDef, Buffer>, z.ZodType<Uint8Array, z.ZodTypeDef, Uint8Array>]>;
+            width: z.ZodNumber;
+            height: z.ZodNumber;
+        }, "strip", z.ZodTypeAny, {
+            data: Buffer | Uint8Array;
+            width: number;
+            height: number;
+        }, {
+            data: Buffer | Uint8Array;
+            width: number;
+            height: number;
+        }>;
+    }, "strip", z.ZodTypeAny, {
+        bitmap: {
+            data: Buffer | Uint8Array;
+            width: number;
+            height: number;
+        };
+    }, {
+        bitmap: {
+            data: Buffer | Uint8Array;
+            width: number;
+            height: number;
+        };
+    }>;
+    /** the x position to draw the image */
+    x: z.ZodOptional<z.ZodNumber>;
+    /** the y position to draw the image */
+    y: z.ZodOptional<z.ZodNumber>;
+}, "strip", z.ZodTypeAny, {
+    src: {
+        bitmap: {
+            data: Buffer | Uint8Array;
+            width: number;
+            height: number;
+        };
+    };
+    x?: number | undefined;
+    y?: number | undefined;
+}, {
+    src: {
+        bitmap: {
+            data: Buffer | Uint8Array;
+            width: number;
+            height: number;
+        };
+    };
+    x?: number | undefined;
+    y?: number | undefined;
+}>]>;
+export type MaskOptions = z.infer<typeof MaskOptionsSchema>;
 export declare const methods: {
     /**
-     * Calculates the perceptual hash
-     * @returns the perceptual hash
+     * Masks a source image on to this image using average pixel colour. A completely black pixel on the mask will turn a pixel in the image completely transparent.
+     * @param src the source Jimp instance
+     * @param x the horizontal position to blit the image
+     * @param y the vertical position to blit the image
      * @example
      * ```ts
      * import { Jimp } from "jimp";
      *
      * const image = await Jimp.read("test/image.png");
+     * const mask = await Jimp.read("test/mask.png");
      *
-     * image.hash();
+     * image.mask(mask);
      * ```
      */
-    pHash<I extends JimpClass>(image: I): string;
-    /**
-     * Generates a perceptual hash of the image <https://en.wikipedia.org/wiki/Perceptual_hashing>. And pads the string. Can configure base.
-     * @param base A number between 2 and 64 representing the base for the hash (e.g. 2 is binary, 10 is decimal, 16 is hex, 64 is base 64). Defaults to 64.
-     * @example
-     * ```ts
-     * import { Jimp } from "jimp";
-     *
-     * const image = await Jimp.read("test/image.png");
-     *
-     * image.hash(2); // binary
-     * image.hash(64); // base 64
-     * ```
-     */
-    hash<I extends JimpClass>(image: I, base?: number): string;
-    /**
-     * Calculates the hamming distance of the current image and a hash based on their perceptual hash
-     * @param compareHash hash to compare to
-     * @returns  a number ranging from 0 to 1, 0 means they are believed to be identical
-     * @example
-     * ```ts
-     * import { Jimp } from "jimp";
-     *
-     * const image = await Jimp.read("test/image.png");
-     *
-     * image.distanceFromHash(image.pHash());
-     * ```
-     */
-    distanceFromHash<I extends JimpClass>(image: I, compareHash: string): number;
+    mask<I extends JimpClass>(image: I, options: MaskOptions): I;
 };
-/**
- * Calculates the hamming distance of two images based on their perceptual hash
- * @param img1 A Jimp image to compare
- * @param img2 A Jimp image to compare
- * @returns A number ranging from 0 to 1, 0 means they are believed to be identical
- * @example
- * ```ts
- * import { Jimp, distance } from "jimp";
- *
- * const image1 = await Jimp.read("test/image.png");
- * const image2 = await Jimp.read("test/image.png");
- *
- * distance(image1, image2); // 0.5
- * ```
- */
-export declare function distance<I extends JimpClass>(img1: I, img2: I): number;
-/**
- * Calculates the hamming distance of two images based on their perceptual hash
- * @param hash1 A pHash
- * @param hash2 A pHash
- * @returns A number ranging from 0 to 1, 0 means they are believed to be identical
- * @example
- * ```ts
- * import { Jimp, compareHashes } from "jimp";
- *
- * const image1 = await Jimp.read("test/image.png");
- * const image2 = await Jimp.read("test/image.png");
- *
- * compareHashes(image1.pHash(), image2.pHash()); // 0.5
- * ```
- */
-export declare function compareHashes(hash1: string, hash2: string): number;
+export {};
 //# sourceMappingURL=index.d.ts.map
--- index.test.ts
+++ index.test.ts
@@ -1,75 +1,95 @@
 import { expect, test, describe } from "vitest";
-import { getTestImagePath } from "@jimp/test-utils";
+
+import { makeTestImage } from "@jimp/test-utils";
 import { createJimp } from "@jimp/core";
-import bmp from "@jimp/js-bmp";
-import png from "@jimp/js-png";
-import jpeg from "@jimp/js-jpeg";
-import tiff from "@jimp/js-tiff";
 
-import { methods, compareHashes, distance } from "./index.js";
+import { methods } from "./index.js";
 
-const Jimp = createJimp({
-  formats: [bmp, png, jpeg, tiff],
-  plugins: [methods],
-});
+const jimp = createJimp({ plugins: [methods] });
 
-describe("hash", () => {
-  test("base 2", async () => {
-    const image = await Jimp.read(getTestImagePath("dice.png"));
+describe("Mask", () => {
+  const imgSrcOpaq = jimp.fromBitmap(
+    // prettier-ignore
+    makeTestImage(
+      "▴□▾□■□",
+      "■▴■▾■□",
+      "■□▴□▾□",
+      "■□■▴■▾"
+    ),
+  );
+  const imgSrcAlpa = jimp.fromBitmap(
+    // prettier-ignore
+    makeTestImage(
+      "▴▵▾▿",
+      "▴▵▾▿",
+      "▴▵▾▿"
+    ),
+  );
+  const maskGrayBig = jimp.fromBitmap(
+    // prettier-ignore
+    makeTestImage(
+      "048840",
+      "8CFFC8",
+      "8CFFC8",
+      "048840"
+    ),
+  );
+  const maskGraySmall = jimp.fromBitmap(
+    // prettier-ignore
+    makeTestImage(
+      "0369",
+      "369C",
+      "69CF"
+    ),
+  );
+  const maskColor = jimp.fromBitmap(
+    // prettier-ignore
+    makeTestImage(
+      "▴▴▾▾",
+      "▪▪▰▰",
+      "□□□□"
+    ),
+  );
 
-    expect(image.hash(2)).toBe(
-      "1100010000011111011010111110000000010101001011010101101000010010",
-    );
+  test("Affect opaque image with a gray mask with the same size", () => {
+    expect(imgSrcOpaq.clone().mask(maskGrayBig)).toMatchSnapshot();
   });
 
-  test("base 10 (decimal)", async () => {
-    const image = await Jimp.read(getTestImagePath("cops.jpg"));
-
-    expect(image.hash(10)).toBe("13442314021806033441");
+  test("Affect opaque image with a gray mask with the same size, blited", () => {
+    expect(
+      imgSrcOpaq.clone().mask({ src: maskGrayBig, x: 1, y: 1 }),
+    ).toMatchSnapshot();
   });
 
-  test("base 16 (hex)", async () => {
-    const image = await Jimp.read(getTestImagePath("rgb.tiff"));
-
-    expect(image.hash(16)).toBe("949800481007044c");
+  test("Affect opaque image with a gray mask with the same size, blited negative", () => {
+    expect(
+      imgSrcOpaq.clone().mask({ src: maskGrayBig, x: -1, y: -1 }),
+    ).toMatchSnapshot();
   });
 
-  test("base 64", async () => {
-    const image = await Jimp.read(getTestImagePath("windows95.bmp"));
-
-    expect(image.hash(64)).toBe("fb0xj0Mw400");
+  test("Affect opaque image with a smaller gray mask", () => {
+    expect(imgSrcOpaq.clone().mask(maskGraySmall)).toMatchSnapshot();
   });
 
-  test("base 23", async function () {
-    const image = await Jimp.read(getTestImagePath("panoramic.jpg"));
-
-    expect(image.hash(23)).toBe("0m1m2id7l7cl4fb");
+  test("Affect opaque image with a smaller gray mask, blited", () => {
+    expect(
+      imgSrcOpaq.clone().mask({ src: maskGraySmall, x: 1, y: 1 }),
+    ).toMatchSnapshot();
   });
 
-  test("base 17", async () => {
-    const image = await Jimp.read(getTestImagePath("dice.png"));
-
-    expect(image.hash(17)).toBe("4ffe16bf68eee351");
-  });
-});
-
-describe("pHash", () => {
-  test("should calculate the distance using distanceFromHash", async function () {
-    const image1 = await Jimp.read(getTestImagePath("dice.png"));
-    const image2 = await Jimp.read(getTestImagePath("mask.png"));
-
-    const hash = image1.pHash();
-
-    expect(image2.distanceFromHash(hash)).toBe(distance(image1, image2));
+  test("Affect alpha image with a bigger gray mask", () => {
+    expect(imgSrcAlpa.clone().mask(maskGrayBig)).toMatchSnapshot();
   });
 
-  test("should calculate the distance using compareHashes.", async function () {
-    const image1 = await Jimp.read(getTestImagePath("dice.png"));
-    const image2 = await Jimp.read(getTestImagePath("mask.png"));
+  test("Affect alpha image with a bigger gray mask, blited", () => {
+    expect(
+      imgSrcAlpa.clone().mask({ src: maskGrayBig, x: -1, y: -1 }),
+    ).toMatchSnapshot();
+  });
 
-    const hash1 = image1.pHash();
-    const hash2 = image2.pHash();
-
-    expect(compareHashes(hash1, hash2)).toBe(distance(image1, image2));
+  test("Affect opaque image with a colored mask", () => {
+    expect(
+      imgSrcOpaq.clone().mask({ src: maskColor, x: 1, y: 1 }),
+    ).toMatchSnapshot();
   });
 });
