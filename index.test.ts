fe4c0b191f850e3874a50f1c124eba4fe7cb73f3
PriscilaRonche <priscila.ronche@gmail.com>
2026-08-21 15:09:18 -0300

    Add files via upload

--- index.test.ts
+++ index.test.ts
@@ -1,63 +1,75 @@
 import { expect, test, describe } from "vitest";
 
-import { makeDonutTestImage } from "@jimp/test-utils";
-import { createJimp } from "@jimp/core";
+import { makeTestImage } from "@jimp/test-utils";
+import { HorizontalAlign, VerticalAlign, createJimp } from "@jimp/core";
 
-import { methods as color } from "./index.js";
+import { methods } from "./index.js";
 
-const jimp = createJimp({ plugins: [color] });
+const jimp = createJimp({ plugins: [methods] });
 
-describe("canvas color transformation", () => {
-  const redDonutJGD = makeDonutTestImage(0x00000000, 0xff000088, 0xff0000ff);
+describe("All align combinations for contain", () => {
+  const vertical = jimp.fromBitmap(
+    makeTestImage(
+      "▴▴▴▴▸▸▸▸",
+      "▴▴▴▴▸▸▸▸",
+      "▴▴▴▴▸▸▸▸",
+      "▴▴▴▴▸▸▸▸",
+      "▴▴▴▴▸▸▸▸",
+      "▴▴▴▴▸▸▸▸",
+      "▾▾▾▾◆◆◆◆",
+      "▾▾▾▾◆◆◆◆",
+      "▾▾▾▾◆◆◆◆",
+      "▾▾▾▾◆◆◆◆",
+      "▾▾▾▾◆◆◆◆",
+      "▾▾▾▾◆◆◆◆",
+    ),
+  );
+  // stores the Jimp instances of the JGD images above.
+  const horizontal = jimp.fromBitmap(
+    makeTestImage(
+      "▴▴▴▴▴▴▸▸▸▸▸▸",
+      "▴▴▴▴▴▴▸▸▸▸▸▸",
+      "▴▴▴▴▴▴▸▸▸▸▸▸",
+      "▴▴▴▴▴▴▸▸▸▸▸▸",
+      "▾▾▾▾▾▾◆◆◆◆◆◆",
+      "▾▾▾▾▾▾◆◆◆◆◆◆",
+      "▾▾▾▾▾▾◆◆◆◆◆◆",
+      "▾▾▾▾▾▾◆◆◆◆◆◆",
+    ),
+  );
 
-  test("can apply more than one color transformation", () => {
-    const image = jimp.fromBitmap(redDonutJGD);
-    const newJGD = image.color([
-      { apply: "hue", params: [-180] },
-      { apply: "lighten", params: [25] },
-    ]);
+  const tests: Array<
+    [keyof typeof HorizontalAlign, keyof typeof VerticalAlign]
+  > = [
+    ["LEFT", "TOP"],
+    ["CENTER", "TOP"],
+    ["RIGHT", "TOP"],
+    ["LEFT", "MIDDLE"],
+    ["CENTER", "MIDDLE"],
+    ["RIGHT", "MIDDLE"],
+    ["LEFT", "BOTTOM"],
+    ["CENTER", "BOTTOM"],
+    ["RIGHT", "BOTTOM"],
+  ];
 
-    expect(newJGD.bitmap).toMatchSnapshot();
-  });
+  tests.forEach(([horizontalAlign, verticalAlign]) => {
+    const align = horizontalAlign + " " + verticalAlign;
+    const alignValue =
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      (HorizontalAlign as any)[horizontalAlign] |
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      (VerticalAlign as any)[verticalAlign];
 
-  test("lighten", () => {
-    const image = jimp.fromBitmap(redDonutJGD);
-    expect(image.color([{ apply: "lighten", params: [25] }])).toMatchSnapshot();
-  });
+    test("vertical contain aligned to " + align, () => {
+      expect(
+        vertical.clone().contain({ w: 6, h: 6, align: alignValue }),
+      ).toMatchSnapshot();
+    });
 
-  test("brighten", () => {
-    const image = jimp.fromBitmap(redDonutJGD);
-    expect(
-      image.color([{ apply: "brighten", params: [25] }]),
-    ).toMatchSnapshot();
-  });
-
-  test("spin hue", () => {
-    const image = jimp.fromBitmap(redDonutJGD);
-    expect(image.color([{ apply: "hue", params: [150] }])).toMatchSnapshot();
-  });
-});
-
-describe("brightness", () => {
-  test("brightness of 1 should be the same as no change", () => {
-    const jgd = makeDonutTestImage(0xff000088, 0xff000088, 0xff000088);
-    const image = jimp.fromBitmap(jgd);
-    const image2 = jimp.fromBitmap(jgd);
-
-    expect(image.brightness(1).bitmap).toStrictEqual(image2.bitmap);
-  });
-
-  test("should be able to brighten", () => {
-    const jgd = makeDonutTestImage(0xaa000088, 0xaa000088, 0xaa000088);
-    const image = jimp.fromBitmap(jgd);
-
-    expect(image.brightness(2).bitmap).toMatchSnapshot();
-  });
-
-  test("should be able to darken", () => {
-    const jgd = makeDonutTestImage(0xaa000088, 0xaa000088, 0xaa000088);
-    const image = jimp.fromBitmap(jgd);
-
-    expect(image.brightness(0.2).bitmap).toMatchSnapshot();
+    test("horizontal contain aligned to " + align, () => {
+      expect(
+        horizontal.clone().contain({ w: 6, h: 6, align: alignValue }),
+      ).toMatchSnapshot();
+    });
   });
 });
