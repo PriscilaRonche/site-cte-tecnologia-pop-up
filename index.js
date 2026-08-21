8184c8008dd08f4c5fc4ee934b18115c0405fb6f
PriscilaRonche <priscila.ronche@gmail.com>
2026-08-21 15:03:51 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,88 +1,207 @@
-import { JimpClassSchema } from "@jimp/types";
-import { limit255, scan } from "@jimp/utils";
-import { z } from "zod";
-const BlitOptionsSchemaComplex = z.object({
-    src: JimpClassSchema,
-    /** the x position to blit the image */
-    x: z.number().optional(),
-    /** the y position to blit the image */
-    y: z.number().optional(),
-    /** the x position from which to crop the source image */
-    srcX: z.number().optional(),
-    /** the y position from which to crop the source image */
-    srcY: z.number().optional(),
-    /** the width to which to crop the source image */
-    srcW: z.number().optional(),
-    /** the height to which to crop the source image */
-    srcH: z.number().optional(),
-});
-const BlitOptionsSchema = z.union([JimpClassSchema, BlitOptionsSchemaComplex]);
+import { mulTable, shgTable } from "./blur-tables.js";
+import { limit255 } from "@jimp/utils";
+/*
+    Superfast Blur (0.5)
+    http://www.quasimondo.com/BoxBlurForCanvas/FastBlur.js
+
+    Copyright (c) 2011 Mario Klingemann
+
+    Permission is hereby granted, free of charge, to any person
+    obtaining a copy of this software and associated documentation
+    files (the "Software"), to deal in the Software without
+    restriction, including without limitation the rights to use,
+    copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following
+    conditions:
+
+    The above copyright notice and this permission notice shall be
+    included in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+    OTHER DEALINGS IN THE SOFTWARE.
+*/
 export const methods = {
     /**
-     * Short for "bit-block transfer".
-     * It involves the transfer of a block of pixel data from one area of a computer's memory to another area, typically for the purpose of rendering images on the screen or manipulating them in various ways.
-     * It's a fundamental operation in computer graphics utilized in various applications, from operating systems to video games.
-     *
+     * A fast blur algorithm that produces similar effect to a Gaussian blur - but MUCH quicker
+     * @param r the pixel radius of the blur
      * @example
      * ```ts
      * import { Jimp } from "jimp";
      *
      * const image = await Jimp.read("test/image.png");
-     * const parrot = await Jimp.read("test/party-parrot.png");
      *
-     * image.blit({ src: parrot, x: 10, y: 10 });
+     * image.blur(5);
      * ```
      */
-    blit(image, options) {
-        const parsed = BlitOptionsSchema.parse(options);
-        let { 
-        // eslint-disable-next-line prefer-const
-        src, x = 0, y = 0, srcX = 0, srcY = 0, srcW = src.bitmap.width, srcH = src.bitmap.height, } = "bitmap" in parsed ? { src: parsed } : parsed;
-        if (!("bitmap" in src)) {
-            throw new Error("The source must be a Jimp image");
+    blur(image, r) {
+        if (typeof r !== "number") {
+            throw new Error("r must be a number");
         }
-        if (typeof x !== "number" || typeof y !== "number") {
-            throw new Error("x and y must be numbers");
+        if (r < 1) {
+            throw new Error("r must be greater than 0");
         }
-        // round input
-        x = Math.round(x);
-        y = Math.round(y);
-        // round input
-        srcX = Math.round(srcX);
-        srcY = Math.round(srcY);
-        srcW = Math.round(srcW);
-        srcH = Math.round(srcH);
-        const maxWidth = image.bitmap.width;
-        const maxHeight = image.bitmap.height;
-        scan(src, srcX, srcY, srcW, srcH, function (sx, sy, idx) {
-            const xOffset = x + sx - srcX;
-            const yOffset = y + sy - srcY;
-            if (xOffset >= 0 &&
-                yOffset >= 0 &&
-                maxWidth - xOffset > 0 &&
-                maxHeight - yOffset > 0) {
-                const dstIdx = image.getPixelIndex(xOffset, yOffset);
-                const srcColor = {
-                    r: src.bitmap.data[idx] || 0,
-                    g: src.bitmap.data[idx + 1] || 0,
-                    b: src.bitmap.data[idx + 2] || 0,
-                    a: src.bitmap.data[idx + 3] || 0,
-                };
-                const dst = {
-                    r: image.bitmap.data[dstIdx] || 0,
-                    g: image.bitmap.data[dstIdx + 1] || 0,
-                    b: image.bitmap.data[dstIdx + 2] || 0,
-                    a: image.bitmap.data[dstIdx + 3] || 0,
-                };
-                image.bitmap.data[dstIdx] =
-                    ((srcColor.a * (srcColor.r - dst.r) - dst.r + 255) >> 8) + dst.r;
-                image.bitmap.data[dstIdx + 1] =
-                    ((srcColor.a * (srcColor.g - dst.g) - dst.g + 255) >> 8) + dst.g;
-                image.bitmap.data[dstIdx + 2] =
-                    ((srcColor.a * (srcColor.b - dst.b) - dst.b + 255) >> 8) + dst.b;
-                image.bitmap.data[dstIdx + 3] = limit255(dst.a + srcColor.a);
+        let rsum;
+        let gsum;
+        let bsum;
+        let asum;
+        let x;
+        let y;
+        let i;
+        let p;
+        let p1;
+        let p2;
+        let yp;
+        let yi;
+        let yw;
+        const wm = image.bitmap.width - 1;
+        const hm = image.bitmap.height - 1;
+        // const wh = image.bitmap.width * image.bitmap.height;
+        const rad1 = r + 1;
+        const mulSum = mulTable[r];
+        const shgSum = shgTable[r];
+        const red = [];
+        const green = [];
+        const blue = [];
+        const alpha = [];
+        const vmin = [];
+        const vmax = [];
+        let iterations = 2;
+        while (iterations-- > 0) {
+            yi = 0;
+            yw = 0;
+            for (y = 0; y < image.bitmap.height; y++) {
+                rsum = image.bitmap.data[yw] * rad1;
+                gsum = image.bitmap.data[yw + 1] * rad1;
+                bsum = image.bitmap.data[yw + 2] * rad1;
+                asum = image.bitmap.data[yw + 3] * rad1;
+                for (i = 1; i <= r; i++) {
+                    p = yw + ((i > wm ? wm : i) << 2);
+                    rsum += image.bitmap.data[p++];
+                    gsum += image.bitmap.data[p++];
+                    bsum += image.bitmap.data[p++];
+                    asum += image.bitmap.data[p];
+                }
+                for (x = 0; x < image.bitmap.width; x++) {
+                    red[yi] = rsum;
+                    green[yi] = gsum;
+                    blue[yi] = bsum;
+                    alpha[yi] = asum;
+                    if (y === 0) {
+                        vmin[x] = ((p = x + rad1) < wm ? p : wm) << 2;
+                        vmax[x] = (p = x - r) > 0 ? p << 2 : 0;
+                    }
+                    p1 = yw + vmin[x];
+                    p2 = yw + vmax[x];
+                    rsum += image.bitmap.data[p1++] - image.bitmap.data[p2++];
+                    gsum += image.bitmap.data[p1++] - image.bitmap.data[p2++];
+                    bsum += image.bitmap.data[p1++] - image.bitmap.data[p2++];
+                    asum += image.bitmap.data[p1] - image.bitmap.data[p2++];
+                    yi++;
+                }
+                yw += image.bitmap.width << 2;
             }
-        });
+            for (x = 0; x < image.bitmap.width; x++) {
+                yp = x;
+                rsum = red[yp] * rad1;
+                gsum = green[yp] * rad1;
+                bsum = blue[yp] * rad1;
+                asum = alpha[yp] * rad1;
+                for (i = 1; i <= r; i++) {
+                    yp += i > hm ? 0 : image.bitmap.width;
+                    rsum += red[yp];
+                    gsum += green[yp];
+                    bsum += blue[yp];
+                    asum += alpha[yp];
+                }
+                yi = x << 2;
+                for (y = 0; y < image.bitmap.height; y++) {
+                    image.bitmap.data[yi] = limit255((rsum * mulSum) >>> shgSum);
+                    image.bitmap.data[yi + 1] = limit255((gsum * mulSum) >>> shgSum);
+                    image.bitmap.data[yi + 2] = limit255((bsum * mulSum) >>> shgSum);
+                    image.bitmap.data[yi + 3] = limit255((asum * mulSum) >>> shgSum);
+                    if (x === 0) {
+                        vmin[y] = ((p = y + rad1) < hm ? p : hm) * image.bitmap.width;
+                        vmax[y] = (p = y - r) > 0 ? p * image.bitmap.width : 0;
+                    }
+                    p1 = x + vmin[y];
+                    p2 = x + vmax[y];
+                    rsum += red[p1] - red[p2];
+                    gsum += green[p1] - green[p2];
+                    bsum += blue[p1] - blue[p2];
+                    asum += alpha[p1] - alpha[p2];
+                    yi += image.bitmap.width << 2;
+                }
+            }
+        }
+        return image;
+    },
+    // http://blog.ivank.net/fastest-gaussian-blur.html
+    /**
+     * Applies a true Gaussian blur to the image (warning: this is VERY slow)
+     * @param r the pixel radius of the blur
+     * @example
+     * ```ts
+     * import { Jimp } from "jimp";
+     *
+     * const image = await Jimp.read("test/image.png");
+     *
+     * image.gaussian(15);
+     * ```
+     */
+    gaussian(image, r) {
+        if (typeof r !== "number") {
+            throw new Error("r must be a number");
+        }
+        if (r < 1) {
+            throw new Error("r must be greater than 0");
+        }
+        const rs = Math.ceil(r * 2.57); // significant radius
+        const range = rs * 2 + 1;
+        const rr2 = r * r * 2;
+        const rr2pi = rr2 * Math.PI;
+        const weights = [];
+        for (let y = 0; y < range; y++) {
+            const weightsRow = [];
+            for (let x = 0; x < range; x++) {
+                const dsq = (x - rs) ** 2 + (y - rs) ** 2;
+                weightsRow[x] = Math.exp(-dsq / rr2) / rr2pi;
+            }
+            weights.push(weightsRow);
+        }
+        for (let y = 0; y < image.bitmap.height; y++) {
+            for (let x = 0; x < image.bitmap.width; x++) {
+                let red = 0;
+                let green = 0;
+                let blue = 0;
+                let alpha = 0;
+                let wsum = 0;
+                for (let iy = 0; iy < range; iy++) {
+                    for (let ix = 0; ix < range; ix++) {
+                        const x1 = Math.min(image.bitmap.width - 1, Math.max(0, ix + x - rs));
+                        const y1 = Math.min(image.bitmap.height - 1, Math.max(0, iy + y - rs));
+                        const weight = weights[iy][ix];
+                        const idx = (y1 * image.bitmap.width + x1) << 2;
+                        red += image.bitmap.data[idx] * weight;
+                        green += image.bitmap.data[idx + 1] * weight;
+                        blue += image.bitmap.data[idx + 2] * weight;
+                        alpha += image.bitmap.data[idx + 3] * weight;
+                        wsum += weight;
+                    }
+                    const idx = (y * image.bitmap.width + x) << 2;
+                    image.bitmap.data[idx] = Math.round(red / wsum);
+                    image.bitmap.data[idx + 1] = Math.round(green / wsum);
+                    image.bitmap.data[idx + 2] = Math.round(blue / wsum);
+                    image.bitmap.data[idx + 3] = Math.round(alpha / wsum);
+                }
+            }
+        }
         return image;
     },
 };
--- index.node.test.ts
+++ index.node.test.ts
@@ -1,86 +1,17 @@
-import { expect, test, describe } from "vitest";
+import { describe, expect, test } from "vitest";
 import { createJimp } from "@jimp/core";
-import jpeg from "@jimp/js-jpeg";
-import png from "@jimp/js-png";
-import "@jimp/test-utils/image-snapshot";
 import { getTestImagePath } from "@jimp/test-utils";
+import "@jimp/test-utils/image-snapshot";
+import png from "@jimp/js-png";
 
-import * as blit from "./index.js";
+import * as blur from "./index.js";
 
-const Jimp = createJimp({ formats: [jpeg, png], plugins: [blit.methods] });
+const Jimp = createJimp({ formats: [png], plugins: [blur.methods] });
 
-describe("Blit over image", function () {
-  test("blit alpha", async () => {
-    const dice = await Jimp.read(getTestImagePath("dice.png"));
-    const image = await Jimp.read(getTestImagePath("cops.jpg"));
-    const output = await image.blit({ src: dice }).getBuffer("image/png");
-
+describe("hasAlpha", () => {
+  test("image with alpha", async () => {
+    const image = await Jimp.read(getTestImagePath("dice.png"));
+    const output = await image.blur(16).getBuffer("image/png");
     expect(output).toMatchImageSnapshot();
-  });
-
-  async function createCat(catNum: number, len: number) {
-    const imgHeight = 60;
-
-    const butt = await Jimp.read(__dirname + "/images/cat_butt.png");
-    const head = await Jimp.read(__dirname + "/images/cat_head.png");
-    const fuzz = await Jimp.read(__dirname + "/images/cat_fuzz.png");
-
-    let longCat = len;
-    longCat = longCat > 20 ? 20 : longCat;
-    longCat = longCat <= 1 ? 1 : longCat;
-
-    const cat =
-      Math.floor(catNum * (head.bitmap.height / imgHeight)) * imgHeight;
-
-    const newImage = new Jimp({
-      width:
-        butt.bitmap.width + head.bitmap.width + fuzz.bitmap.width * longCat,
-      height: imgHeight,
-      color: 0x00000000,
-    });
-
-    newImage.blit({
-      src: butt,
-      x: 0,
-      y: 0,
-      srcX: 0,
-      srcY: cat,
-      srcW: butt.bitmap.width,
-      srcH: imgHeight,
-    });
-    for (let i = 0; i < longCat; i++) {
-      newImage.blit({
-        src: fuzz,
-        x: butt.bitmap.width + fuzz.bitmap.width * i,
-        y: 0,
-        srcX: 0,
-        srcY: cat,
-        srcW: fuzz.bitmap.width,
-        srcH: imgHeight,
-      });
-    }
-
-    newImage.blit({
-      src: head,
-      x: butt.bitmap.width + fuzz.bitmap.width * longCat,
-      y: 0,
-      srcX: 0,
-      srcY: cat,
-      srcW: head.bitmap.width,
-      srcH: imgHeight,
-    });
-
-    return newImage;
-  }
-
-  test("uses src params correctly", async () => {
-    const small = await createCat(0.3, 1);
-    expect(await small.getBuffer("image/png")).toMatchImageSnapshot();
-
-    const medium = await createCat(0.6, 7);
-    expect(await medium.getBuffer("image/png")).toMatchImageSnapshot();
-
-    const large = await createCat(0.9, 20);
-    expect(await large.getBuffer("image/png")).toMatchImageSnapshot();
   });
 });
